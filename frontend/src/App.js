59baf1d2f86c9265da7bff5a37ed6da28739f816
Josh Millard <josh.millard10@gmail.com>
2021-12-12 09:21:22 -0500

    frontend axios glitch.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,13 +6,11 @@ import axios from 'axios';
 function App() {
   const [state, setState] = useState('default')
   axios.get(`http://localhost:3000/api/users/1/shelves`).then (res => {
-    console.log(res.data)
-    setState(res)
+    
   })
   return (
     <div className="App">
       <p>MY SITE</p>
-      {state}
     </div>
   );
 }
